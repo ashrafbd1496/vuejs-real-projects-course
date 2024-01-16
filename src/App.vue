91ede3bca29519ae92f1adc3ca34bc27527fb703
ashrafbd1496 <asraf1496@gmail.com>
2024-01-16 07:46:46 +0600

    t
    First commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <div>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/logo.png" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vue Vite Tailwind Boilerplate" />
+</template>
+
+<style scoped>
+.logo {
+  width: 50rem;
+  height: auto;
+  padding: 1.5em;
+  padding-bottom: 0;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
